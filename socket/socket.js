c96113998877ed09af26770a1ac4f9a3a29e9719
subash <subash1327@icloud.com>
2022-03-23 04:27:37 +0530

    added user online offline apis

--- socket/socket.js
+++ socket/socket.js
@@ -1,5 +1,6 @@
 const socket_io = require('socket.io');
 const io = socket_io();
+const knex = require('../knex')
 const socketApi = {};
 
 
@@ -13,7 +14,44 @@ io.on('connection', socket => {
     
     socket.on('disconnect', () => {
         console.warn(`disconnected - ${socket.id}`)
+        let data = users[socket.id]
+        if(data){
+            io.emit(`org::${data.org.id}::offline`, data.user)
+            knex.update('con_stat', {
+                fields: {
+                    alive: false,
+                    date: 'NOW()'
+                },
+                conditions: [
+                    ['uid', '=', data.user.id]
+                ]
+            })
+            delete users[socket.id];
+        }
+        
     });
+    
+    socket.on('online', async (data) => {
+        users[socket.id] = data
+        io.emit(`org::${data.org.id}::online`, data.user)
+        try {
+            knex.update('con_stat', {
+                fields: {
+                    alive: true,
+                    date: 'NOW()'
+                },
+                conditions: [
+                    ['uid', '=', data.user.id]
+                ]
+            })
+        } catch (e) {
+            knex.insert('con_stat', {
+                uid: data.user.id,
+                oid: data.org.id
+            })
+        }
+    });
+
     socket.on('event', (data) => {
         console.log(`event - ${JSON.stringify(data)}`)
         io.emit(data.event, data.data)
